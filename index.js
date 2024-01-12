5b35c7703ccf96dfd83cabcea374c2ff0cede74d
phani kumar <phanikumar21g@gmail.com>
2024-01-12 13:31:26 +0530

    ignore

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const element=React.createElement("div",{id:"parent"},[React.createElement("h1",{id:"sibling"},"child one"),React.createElement("h2",{id:"sibling"},"child two")]);
 const root=ReactDOM.createRoot(document.getElementById("test"));
 root.render(element);
