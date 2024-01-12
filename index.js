fc86a396dc0a6759900b53ca603bba8ba8e8247c
phani kumar <phanikumar21g@gmail.com>
2024-01-12 12:19:37 +0530

    rendering

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-const element=React.createElement("div",{id:parent},[React.createElement("h1",{id:sibling},"child one"),React.createElement("h2",{id:sibling},"child two")])
+const element=React.createElement("div",{id:parent},[React.createElement("h1",{id:sibling},"child one"),React.createElement("h2",{id:sibling},"child two")]);
+const root=ReactDOM.createRoot(document.getElementById("test"));
+root.render(element);
